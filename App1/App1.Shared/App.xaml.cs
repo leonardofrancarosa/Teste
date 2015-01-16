6673c2959656a64ebbb84c4bd76793b677d72652
leonardofrancarosa <leonardofrancarosa@ig.com.br>
2015-01-16 21:37:43 -0200

    testesss

--- App1/App1.Shared/App.xaml.cs
+++ App1/App1.Shared/App.xaml.cs
@@ -36,7 +36,7 @@ namespace App1
         public App()
         {
             this.InitializeComponent();
-            this.Suspending += this.OnSuspending; //Leonardo França rosa dddddffdf
+            this.Suspending += this.OnSuspending; //Leonardo França rosa dddddffdf jjhkjhkjh
         }
 
         /// <summary>
